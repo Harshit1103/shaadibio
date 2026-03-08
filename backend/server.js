9c62275b5d8c8a3f3e05ce5b51a6d94540842def
Harshit Kumar <harshitkumar1304@gmail.com>
2026-03-08 22:04:26 +0530

    fixed duplicate cores

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
-import cors from "cors";
+
 
 import authRoutes from "./routes/authRoutes.js"
 import biodataRoutes from "./routes/biodataRoutes.js"
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/biodata", biodataRoutes)
 
 
-app.use(cors());
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
